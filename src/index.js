83278ef99ec2ad4835164f41fa981282564649b8
Oscar Tunebing <tunebing@gmail.com>
2019-06-16 17:02:54 +0200

    Remove redirect mode for server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const apiRouter = require('./api');
-const asyncHandler = require('express-async-handler');
-const { findUrl, mongo } = require('./util/db');
 
 const app = express();
 
@@ -9,42 +7,28 @@ const app = express();
 const {
     PORT,
     WEBPACK_DEV,
-    MODE,
-} = require('./env');
+} = process.env;
 
-if(MODE === 'redirect') {
-    app.get('/:hash', asyncHandler(async (req, res) => {
-        await mongo(async db => {
-            const url = await findUrl(db, req.params.hash);
-            if(url) {
-                res.redirect(url);
-            } else {
-                res.redirect('/app');
-            }
-        });
+app.use('/api', apiRouter);
+
+if(WEBPACK_DEV) {
+    // Webpack config.
+    const config = require('../../webpack.dev.js');
+
+    // Require modules.
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+
+    // Create webpack compiler.
+    const compiler = webpack(config);
+
+    // Apply middleware.
+    app.use('/', webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath,
     }));
 } else {
-    app.use('/api', apiRouter);
-
-    if(WEBPACK_DEV) {
-        // Webpack config.
-        const config = require('../../webpack.dev.js');
-    
-        // Require modules.
-        const webpack = require('webpack');
-        const webpackDevMiddleware = require('webpack-dev-middleware');
-    
-        // Create webpack compiler.
-        const compiler = webpack(config);
-    
-        // Apply middleware.
-        app.use('/', webpackDevMiddleware(compiler, {
-            publicPath: config.output.publicPath,
-        }));
-    } else {
-        // Serve static files.
-        app.use('/', express.static('public'));
-    }
+    // Serve static files.
+    app.use('/', express.static('public'));
 }
 
 // Start server.
