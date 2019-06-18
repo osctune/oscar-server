dace1cdc1c6cae9f8956ca3afb71a23bf61b50cd
Oscar Tunebing <tunebing@gmail.com>
2019-06-18 13:06:34 +0200

    Server refactor

--- src/api/index.js
+++ src/api/index.js
@@ -8,42 +8,57 @@ const {
     mongo,
 } = require('@nam3/oscar-db');
 
-const {
-    formatResponse,
-} = require('@nam3/oscar-util/format');
-
+const validateURL = require('@nam3/oscar-util/validateURL');
 const calculateHash = require('@nam3/oscar-util/hash');
 
 const {
     MONGO_URI,
     MONGO_DBNAME,
+    REDIRECT_URL,
 } = process.env;
 
 const connection = mongo(MONGO_URI, MONGO_DBNAME);
 
 const router = express.Router();
 
-router.get('/createUrl/:url', asyncHandler(async (req, res) => {
+router.post('/createUrl/:url', asyncHandler(async (req, res) => {
     const { url } = req.params;
-    await connection(async db => {
-        // Find existing hash.
-        let hash = await findHash(db, url);
-        // If not found.
-        if (!hash) {
-            // Create new unique id in db.
-            const id = await createId(db);
-            // Calculate hash.
-            hash = calculateHash(id);
-            // Add url and hash to db.
-            await addHash(db, url, hash);
-        }
-        // Respond with hash url.
-        res.end(formatResponse(req, hash));
-    });
-}));
 
-router.get('*', (_req, res) => {
-    res.status(404).end();
-});
+    if(!url) {
+        res.status(400).end('No URL provided.');
+    }
+
+    if(url.startsWith(REDIRECT_URL)) {
+        res.status(400).end('Linking to redirect server is not allowed.');
+    }
+
+    try {
+        validateURL(url);
+
+        try {
+            await connection(async db => {
+                // Find existing hash.
+                let hash = await findHash(db, url);
+                // If not found.
+                if (!hash) {
+                    // Create new unique id in db.
+                    const id = await createId(db);
+                    // Calculate hash.
+                    hash = calculateHash(id);
+                    // Add url and hash to db.
+                    await addHash(db, url, hash);
+                }
+                // Respond with hash url.
+                res.end(`${REDIRECT_URL}/${hash}`);
+            });
+        } catch(err) {
+            console.log(err);
+            res.status(500).end();
+            return;
+        }
+    } catch(err) {
+        res.status(400).end('Invalid URL');
+    }
+}));
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./api');
-
-const app = express();
 
 // Get enviorment variables.
 const {
-    PORT=1336,
+    PORT = 1336,
 } = process.env;
 
-app.use('/', apiRouter);
+const app = express();
+app.use(cors());
+
+app.use('/api', apiRouter);
+app.use('*', (req, res) => {
+    res.status(404).end('Not found');
+});
 
 // Start server.
 app.listen(PORT, () => {
